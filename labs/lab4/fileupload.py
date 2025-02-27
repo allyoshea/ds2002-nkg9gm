88a6bda308d96141925964fe3579f13aea2d56bc
allyoshea <nkg9gm@virginia.edu>
2025-02-27 09:31:00 -0500

    Update fileupload.py with print line

--- labs/lab4/fileupload.py
+++ labs/lab4/fileupload.py
@@ -11,3 +11,4 @@ s3.upload_file(file_name, bucket, file_name, ExtraArgs={'ACL': 'public-read'})
 
 # Generate a public URL (S3 static website endpoint or direct link)
 public_url = f"https://{bucket}.s3.amazonaws.com/{file_name}"
+print(public_url)
